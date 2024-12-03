71d8904d47aa3fb0368abe7e2527fffda33136bc
TheSeanicles <psean6@vt.edu>
2024-12-03 13:22:45 -0500

    open firefox

--- script.sh
+++ script.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-for x in {1760..1895..45}; do
-  for y in {760..985..45}; do
-    xdotool mousemove --sync $x $y click 3 sleep 0.1 \
-    mousemove_relative --sync 0 20 click 1 sleep 0.1
-  done
-done
+xdotool search "Mozilla Firefox" windowactivate --sync key --clearmodifiers ctrl+l
