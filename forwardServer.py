d7b5b9660565808ac1c56fb4be0d4946e5070afd
TheSeanicles <psean6@vt.edu>
2024-12-03 14:22:45 -0500

    keras-ocr

--- forwardServer.py
+++ forwardServer.py
@@ -1,5 +1,6 @@
 # https://selenium-python.readthedocs.io/installation.html#installing-python-bindings-for-selenium
 import subprocess
+import keras_ocr
 from mss.linux import MSS as mss
 
 class server:
@@ -9,6 +10,16 @@ class server:
         with mss(display=":0.0") as sct:
             for filename in sct.save():
                 print(filename)
+                pipeline = keras_ocr.pipeline.Pipeline()
+                images = [
+                            keras_ocr.tools.read(img) for img in [filename]
+                        ]
+                # generate text predictions from the images
+                prediction_groups = pipeline.recognize(images)
+                predicted_image = prediction_groups[1]
+                for text, box in predicted_image:
+                    print(text)
+
 
     def run():
         pass
