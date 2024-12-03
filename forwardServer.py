01c9530856d29c32c6cea7eabbc2acb502b1d3c6
TheSeanicles <psean6@vt.edu>
2024-12-03 11:12:24 -0500

    different command

--- forwardServer.py
+++ forwardServer.py
@@ -12,7 +12,7 @@ class server:
         pass
 
     def scratch():
-        run1 = 'xdotool type "neofetch"; xdotool key KP_Enter;'        
+        run1 = 'xdotool key Super+1'        
         subprocess.run(run1, check=True, stdout=subprocess.PIPE, executable="/bin/bash").stdout
         print(run1.stdout)
 
