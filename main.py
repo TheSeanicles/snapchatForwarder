189a8b3d4f9cfd6d18ad1864054037ae1d9b666d
TheSeanicles <psean6@vt.edu>
2024-12-03 13:21:35 -0500

    last attempt on xdotool

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import forwardServer
+import subprocess
+import time
+from multiprocessing import Process
 
 if __name__ == '__main__':
     s = forwardServer.server
     s.run()
+    def f():
+        subprocess.call(['sh', './script.sh'])
+    p = Process(target=f)
+    p.start()
+    time.sleep(5)
+    p.join()
