b8a2a35fa9cb051cb97fb714f548f30c5b092337
TheSeanicles <psean6@vt.edu>
2024-12-03 11:20:59 -0500

    ah .stdout twice

--- forwardServer.py
+++ forwardServer.py
@@ -14,7 +14,7 @@ class server:
     def scratch():
         run1 = 'xdotool search "Mozilla Firefox" windowactivate --sync key --clearmodifiers ctrl+l'
         run1 = 'neofetch'
-        subprocess.run(run1, check=True, stdout=subprocess.PIPE, executable="/bin/bash").stdout
+        subprocess.run(run1, check=True, stdout=subprocess.PIPE, executable="/bin/bash")
         print(run1.stdout)
 
 if __name__ == '__main__':
