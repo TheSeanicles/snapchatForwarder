31ede967b4fd311e562ad4a1870b52622297bc5b
TheSeanicles <psean6@vt.edu>
2024-12-03 11:24:37 -0500

    list?

--- forwardServer.py
+++ forwardServer.py
@@ -12,7 +12,7 @@ class server:
         pass
 
     def scratch():
-        run1 = ['xdotool', 'search "Mozilla Firefox" windowactivate --sync key --clearmodifiers ctrl+l']
+        run1 = ['xdotool', 'search', '"Mozilla Firefox"', 'windowactivate', '--sync', 'key', '--clearmodifiers', 'ctrl+l']
         subprocess.run(run1, check=True, stdout=subprocess.PIPE, executable="/bin/bash")
         print(run1)
 
