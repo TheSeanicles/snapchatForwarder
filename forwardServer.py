1729f9e9ca3f77bd6bbb0724896ecd2eb043c5f5
TheSeanicles <psean6@vt.edu>
2024-12-03 14:27:56 -0500

    easyocr

--- forwardServer.py
+++ forwardServer.py
@@ -1,6 +1,6 @@
 # https://selenium-python.readthedocs.io/installation.html#installing-python-bindings-for-selenium
 import subprocess
-import keras_ocr
+import easyocr
 from mss.linux import MSS as mss
 
 class server:
@@ -10,15 +10,9 @@ class server:
         with mss(display=":0.0") as sct:
             for filename in sct.save():
                 print(filename)
-                pipeline = keras_ocr.pipeline.Pipeline()
-                images = [
-                            keras_ocr.tools.read(img) for img in [filename]
-                        ]
-                # generate text predictions from the images
-                prediction_groups = pipeline.recognize(images)
-                predicted_image = prediction_groups[1]
-                for text, box in predicted_image:
-                    print(text)
+                reader = easyocr.Reader(['en'])
+                result = reader.readtext('filename')
+                print(result)
 
 
     def run():
