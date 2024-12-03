db7dc8ea64adb779ca0596fb32d47497a161afb9
TheSeanicles <psean6@vt.edu>
2024-12-03 10:57:54 -0500

    output on run 1

--- forwardServer.py
+++ forwardServer.py
@@ -15,8 +15,7 @@ class server:
         run1 = 'xdotool type "neofetch"; xdotool key KP_Enter'
         run1 = ('xdotool', 'type "neofetch"')
         run2 = ('xdotool', 'key KP_Enter')
-        subprocess.run(run1, check=True, stdout=subprocess.PIPE).stdout
-        print(run1.stdout)
+        subprocess.run(run1)
         
         subprocess.run(run2, check=True, stdout=subprocess.PIPE).stdout
         print(run2.stdout)
