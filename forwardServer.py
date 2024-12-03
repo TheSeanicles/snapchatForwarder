b42f4cb4c4db2bda27673dc955ec878b772f6ce1
TheSeanicles <psean6@vt.edu>
2024-12-03 10:59:09 -0500

    /bin/bash in subprocess run

--- forwardServer.py
+++ forwardServer.py
@@ -12,13 +12,9 @@ class server:
         pass
 
     def scratch():
-        run1 = 'xdotool type "neofetch"; xdotool key KP_Enter'
-        run1 = ('xdotool', 'type "neofetch"')
-        run2 = ('xdotool', 'key KP_Enter')
-        subprocess.run(run1)
-        
-        subprocess.run(run2, check=True, stdout=subprocess.PIPE).stdout
-        print(run2.stdout)
+        run1 = 'xdotool type "neofetch"; xdotool key KP_Enter'        
+        subprocess.run(run1, check=True, stdout=subprocess.PIPE, executable="/bin/bash").stdout
+        print(run1.stdout)
 
 if __name__ == '__main__':
     pass
