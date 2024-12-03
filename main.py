58b2e1f8e59407e7fd2f9d4593e47e0c317078ef
TheSeanicles <psean6@vt.edu>
2024-12-03 11:27:29 -0500

    sleep to minimize terminal

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import time
 import forwardServer
 
 if __name__ == '__main__':
     s = forwardServer.server
     s.run()
     s.scratch()
+    time.time.sleep(5)
     # input("Press return to close the program")
