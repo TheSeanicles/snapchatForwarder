b4b0adea033ae060d1fec9022be7a575b2ae9717
TheSeanicles <psean6@vt.edu>
2024-12-03 13:23:52 -0500

    no script.sh file

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     s = forwardServer.server
     s.run()
     def f():
-        subprocess.call(['sh', './script.sh'])
+        subprocess.call(['sh', 'xdotool search "Mozilla Firefox" windowactivate --sync key --clearmodifiers ctrl+l'])
     p = Process(target=f)
     p.start()
     time.sleep(5)
