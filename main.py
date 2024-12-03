dce86537d82d6cf5036b5a54a2fc1d698f50be3e
TheSeanicles <psean6@vt.edu>
2024-12-03 13:45:54 -0500

    do I need process

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from multiprocessing import Process
 if __name__ == '__main__':
     s = forwardServer.server
     s.run()
-    def f():
-        s.scratch()
-    p = Process(target=f)
-    p.start()
-    time.sleep(5)
-    p.join()
+    # def f():
+    #     s.scratch()
+    # p = Process(target=f)
+    # p.start()
+    # time.sleep(5)
+    # p.join()
+    s.scratch()
